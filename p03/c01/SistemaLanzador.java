9ffc67eb2b579d3c81651b40c70559cae125836d
chori <chori@192.168.0.170>
2021-04-15 19:41:56 +0200

    Modificar para que sean 50 antes estaba en 20

--- p03/c01/SistemaLanzador.java
+++ p03/c01/SistemaLanzador.java
@@ -3,7 +3,7 @@ package p03.c01;
 public class SistemaLanzador {
 	public static void main(String[] args) {
 		
-		IParque parque = new Parque(20); // TODO
+		IParque parque = new Parque(50); 
 		char letra_puerta = 'A';
 		
 		System.out.println("¡Parque abierto!");
